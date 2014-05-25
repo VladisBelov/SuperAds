fdf8f8868cd71ede7bdd1e7fd722ddc0674b8bf8
Yaroslav Pogrebnyak <yyyaroslav@gmail.com>
2014-05-25 19:36:06 +0300

    Added some code

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,20 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@EnableTransactionManagement
+@EnableJpaRepositories
 public class Application {
 
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello222");
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
+
 }
+
