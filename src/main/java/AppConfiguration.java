012663bcb415447846db2dab184bb82d87996316
Yaroslav Pogrebnyak <yyyaroslav@gmail.com>
2014-05-25 19:51:06 +0300

    Fixed datasource bugs

---
+++ src/main/java/AppConfiguration.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public javax.sql.DataSource getDataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.postgresql.Driver.class);
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1111");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/SuperAds");
+        return dataSource;
+    }
+}
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,8 +8,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@EnableTransactionManagement
-@EnableJpaRepositories
+//@EnableTransactionManagement
+//@EnableJpaRepositories
 public class Application {
 
     public static void main(String[] args) throws Exception {
